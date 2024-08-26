bea12a2a47172c9d1dd289d1eb97c75249849601
Fazle Alahi <fazlealahi6@gmail.com>
2024-08-26 20:23:43 +0600

    fixed aspect ratio issue

--- src/components/LogoAndName.js
+++ src/components/LogoAndName.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Image from "next/image";
 
-export default function LogoAndName({size = "large"}) {
+export default function LogoAndName({ size = "large" }) {
   return (
     <div className="flex flex-row items-center">
       <Image
-        src="/images/ninjashield2.png"
+        src="/images/ninjashield.png"
         alt="Ninja Shield Logo"
-        width={size=="large" ? 100 : size=="small" ? 50 : 75}
-        height={size=="large" ? 60 : size=="small" ? 30 : 45}
+        width={size == "large" ? 100 : size == "small" ? 50 : 75}
+        height={size == "large" ? 60 : size == "small" ? 30 : 45}
+        style={{ width: 'auto', height: 'auto' }}
+        priority
       />
       <Image
-        className={size=="large" ? "ml-6" : size=="small" ? "ml-2" : "ml-1"}
+        className={size == "large" ? "ml-6" : size == "small" ? "ml-2" : "ml-1"}
         src="/images/text.png"
         alt="Ninja Shield"
-        width={size=="large" ? 350 : size=="small" ? 150 : 250}
-        height={size=="large" ? 20 : size=="small" ? 10 : 15}
+        width={size == "large" ? 350 : size == "small" ? 120 : 250}
+        height={size == "large" ? 20 : size == "small" ? 10 : 15}
+        style={{ width: 'auto', height: 'auto' }}
       />
     </div>
   );
